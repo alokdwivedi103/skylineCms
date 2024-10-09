87f9f4d3f05c187271d74b9dc80e7265f95ff3f3
Alok Dwivedi <alokdwivedi103@gmail.com>
2024-10-09 21:16:02 +0530

    gap fix

--- client/components/Home/TopSellingAuthors.tsx
+++ client/components/Home/TopSellingAuthors.tsx
@@ -6,7 +6,7 @@ export default function TopSellingAuthors() {
   return (
     <section className="text-center">
         <h2 className="text-2xl font-bold text-primaryColor my-6">Top Selling Authors</h2>
-        <div className="flex flex-wrap gap-y-11">
+        <div className="flex flex-wrap gap-x-4 gap-y-11">
           {AUTHORS.map(author => (
             <div key={author.name} className="mx-auto">
               <Image alt={author.name} className="w-24 h-24 object-cover hover:scale-125 transition-all duration-300" height={100} src={author.image} width={100} />
