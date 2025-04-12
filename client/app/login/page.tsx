d8c37367f144c4dee5d80ad2c0f203f6f4abd298
Alok Dwivedi <alok.dwivedi@universityliving.com>
2025-04-12 18:54:09 +0530

    login fix

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -172,7 +172,7 @@ export default function LoginPage() {
                     href="/register"
                     className="font-medium text-primary hover:text-primary/90"
                   >
-                    Don't have an account? Register
+                    Don&apos;t have an account? Register
                   </Link>
                 </div>
               </form>
